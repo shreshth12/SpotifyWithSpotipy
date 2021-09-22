c9b7d7ed67bae680f49df594c3ca70f8487673a6
Ubuntu <azureuser@SWE.qvu2erqcajvuvci5lndo51fpxf.bx.internal.cloudapp.net>
2021-09-22 05:00:16 +0000

    Finished the part where I have the songs and their links

--- get_artist.py
+++ get_artist.py
@@ -2,8 +2,9 @@ from api_key import *
 import spotipy, json
 from spotipy.oauth2 import SpotifyClientCredentials
 
+index = 0
 songs_by_weekend = {}
-filtered_songs = []
+
 
 weekend_uri = 'https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ?si=nDkuIe2zS2KGYbAVTLvHSQ&dl_branch=1'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id = f'{clientID}', client_secret = f'{clientSecret}'))
@@ -11,9 +12,13 @@ spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(cl
 results = spotify.artist_top_tracks(weekend_uri)
 
 for track in results['tracks'][:10]:
-    print()
-    print('track    : ' + track['name'])
-    print()
+    songs_by_weekend[track['name']] = results['tracks'][index]['external_urls']['spotify']
+    # print()
+    # print('track    : ' + track['name'])
+    # print()
+    index += 1
 
-
-print(json.dumps(results, indent = 2))
+for track in songs_by_weekend:
+    print(f'{track} : {songs_by_weekend[track]}')
+    print()
+#print(json.dumps(results, indent = 2))
