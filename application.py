15e211587a52db5edcef1830e21f6a5f7e10c3d3
Ubuntu <azureuser@SWE.qvu2erqcajvuvci5lndo51fpxf.bx.internal.cloudapp.net>
2021-09-22 18:32:30 +0000

    Spotify part complete

--- application.py
+++ application.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, url_for
-from spotify import songs
+from get_artist import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html', songs = songs)
+    return render_template(
+        'index.html', songs_by_weekend = songs_by_weekend,
+        all_pairs = all_pairs, 
+        weekend_image = weekend_image
+        )
 
 if __name__ == '__main__':
     app.run(debug = True)
--- get_artist.py
+++ get_artist.py
@@ -4,32 +4,52 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 index = 0
 songs_by_weekend = {}
-
+weekend_image = ''
 
 weekend_uri = 'https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ?si=nDkuIe2zS2KGYbAVTLvHSQ&dl_branch=1'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id = f'{clientID}', client_secret = f'{clientSecret}'))
 
 results = spotify.artist_top_tracks(weekend_uri)
 
+
 for track in results['tracks'][:10]:
     Values = {}
     Values['link_to_song'] = results['tracks'][index]['external_urls']['spotify']
     Values['link_to_displayPhoto'] = results['tracks'][index]['album']['images'][1]['url']
+    Values['preview'] = track['preview_url']
+
     songs_by_weekend[track['name']] = Values
     # print()
     # print('track    : ' + track['name'])
     # print()
     index += 1
 
+def print_songs():
+    for track in songs_by_weekend:
+        print(track)
+        print(songs_by_weekend[track]['link_to_song'])
+        print(songs_by_weekend[track]['link_to_displayPhoto'])
+        print(songs_by_weekend[track]['preview'])
+        print()
 
-for track in songs_by_weekend:
-    print(track)
-    print(songs_by_weekend[track]['link_to_song'])
-    print(songs_by_weekend[track]['link_to_displayPhoto'])
-    print()
+#Get the artist's picture by his name 
+
+if len(sys.argv) > 1:
+    name = ' '.join(sys.argv[1:])
+else:
+    name = 'The Weekend'
+
+results = spotify.search(q='artist:' + name, type='artist')
+items = results['artists']['items']
+if len(items) > 0:
+    artist = items[0]
+    weekend_image = artist['images'][0]['url']
+
+all_pairs = list(songs_by_weekend.items())
+print_songs()
 
 
-    
+
 #-----------------------------------
 
 #print(json.dumps(results, indent = 2))
