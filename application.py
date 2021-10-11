bba062382289a009f8606e41cc6500f41dd4cb76
Shreshth Kaushik <shreshthsoccer12@gmail.com>
2021-10-10 21:46:31 -0400

    Login/Logout funtionality finished

--- application.py
+++ application.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, url_for, flash, redirect, request
 from get_artist import *
 import random
 import os
-from forms import registrationForm, loginForm
-from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required, UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
@@ -12,15 +11,15 @@ from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationE
 #skaushik2test
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'the random string'
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return 'skaushik2'
+login_manager = LoginManager(app)
+login_manager.login_view = 'login_page'
 app.config['SECRET_KEY'] = 'the random string' 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 from sqlalchemy.orm import backref
 
@@ -42,7 +41,7 @@ class loginForm(FlaskForm):
     submit = SubmitField('Log in')
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
@@ -60,6 +59,7 @@ class Post(db.Model):
         return f"Artist_ID('{self.artist_uri}')"
 
 @app.route("/", methods=['GET', 'POST'])
+@login_required
 def hello_world():
     #artists = [Justin, Weekend, Taylor]
     random_number = random.randint(0,2)
@@ -71,18 +71,19 @@ def login_page():
         return redirect(url_for('hello_world'))
     form = loginForm()
     if form.validate_on_submit():
-        user = form
-        if(form.username.data == 'skaushik2' and form.password.data == 'hello123'):
+        user = User.query.filter_by(username = form.username.data).first()
+        if(user and user.password == form.password.data):
             login_user(user)
             next_page = request.args.get('next')
-            flash('Welcome back!', 'success')
-            return redirect(next_page) if next_page else redirect(url_for('hello_world'))
+            return redirect(url_for('hello_world'))
         else:
             flash('Incorrect username or password', 'error')
     return render_template('login.html', form = form, title = 'login page')
 
 @app.route("/register", methods=['GET', 'POST'])
 def register_page():
+    if current_user.is_authenticated:
+        return redirect(url_for('hello_world'))
     form = registrationForm()
     if form.validate_on_submit():
         user = User(username = form.username.data, password = form.password.data)
@@ -91,6 +92,11 @@ def register_page():
         flash('Your account is now created and you can log in below', 'success')
         return redirect(url_for('login_page'))
     return render_template('register.html', form = form, title = 'Registration page')
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('hello_world'))
 
 if __name__ == '__main__':
     app.run(debug = True, host="0.0.0.0")
